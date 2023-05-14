f2fe61baafedea6c325001129dc9026b5637233e
lowkkid <lowkkidvstheworld@gmail.com>
2023-05-14 20:04:37 +0300

    added DateTImeAPI restaraunts workTime

--- src/main/java/by/fpmibsu/bystro_i_tochka/entity/Restaurants.java
+++ src/main/java/by/fpmibsu/bystro_i_tochka/entity/Restaurants.java
@@ -1,16 +1,34 @@
 package by.fpmibsu.bystro_i_tochka.entity;
 
+import jdk.vm.ci.meta.Local;
+
+import java.time.DayOfWeek;
 import java.util.Calendar;
+import java.time.LocalTime;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Restaurants {
 
-    public Restaurants(Address location, MyCalendar workTime) {
+    public Restaurants(Address location, LocalTime workTimeStart, LocalTime workTimeEnd) {
         this.location = location;
-        this.workTime = workTime;
+        this.workTimeStart = workTimeStart;
+        this.workTimeEnd = workTimeEnd;
     }
+
+    public Restaurants(Address location, LocalTime workTimeStart, LocalTime workTimeEnd, DayOfWeek...weekendDays) {
+        this.location = location;
+        this.workTimeStart = workTimeStart;
+        this.workTimeEnd = workTimeEnd;
+        this.weekends = new HashSet<>();
+        Collections.addAll(weekends, weekendDays);
+    }
+
+
     public Restaurants() {
         this.location = new Address();
-        this.workTime = new MyCalendar();
     }
 
     public Address getLocation() {
@@ -21,15 +39,34 @@ public class Restaurants {
         this.location = location;
     }
 
-    public MyCalendar getWorkTime() {
-        return workTime;
+    public LocalTime getWorkTimeStart() {
+        return workTimeStart;
     }
 
-    public void setWorkTime(MyCalendar workTime) {
-        this.workTime = workTime;
+    public void setWorkTimeStart(LocalTime workTimeStart) {
+        this.workTimeStart = workTimeStart;
+    }
+
+    public LocalTime getWorkTimeEnd() {
+        return workTimeEnd;
+    }
+
+    public void setWorkTimeEnd(LocalTime workTimeEnd) {
+        this.workTimeEnd = workTimeEnd;
+    }
+
+    public Set<DayOfWeek> getWeekends() {
+        return weekends;
+    }
+
+    public void setWeekends(Set<DayOfWeek> weekends) {
+        this.weekends = weekends;
     }
 
     private Address location;
 
-    private MyCalendar workTime;
+    private LocalTime workTimeStart;
+    private LocalTime workTimeEnd;
+
+    private Set<DayOfWeek> weekends;
 }
