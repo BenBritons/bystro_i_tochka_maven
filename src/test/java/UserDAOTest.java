6da1470c7f23046b1a50832bf5195d8641e7d86c
lowkkid <lowkkidvstheworld@gmail.com>
2023-06-01 05:27:20 +0300

    added second test

---
+++ src/test/java/UserDAOTest.java
@@ -0,0 +1,24 @@
+import by.fpmibsu.bystro_i_tochka.DAO.UserDAO;
+import by.fpmibsu.bystro_i_tochka.entity.User;
+import by.fpmibsu.bystro_i_tochka.exeption.DaoException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserDAOTest {
+
+    @Test
+    public void testFindEntityById() throws DaoException {
+        // Создаем экземпляр UserDAO
+        UserDAO userDAO = new UserDAO();
+
+        // Устанавливаем ожидаемый ID пользователя
+        int expectedUserId = 20006;
+
+        // Вызываем метод findEntityById() для получения пользователя по ID
+        User user = userDAO.findEntityById(expectedUserId);
+
+        // Проверяем, что полученный пользователь не равен null и его ID соответствует ожидаемому значению
+        Assert.assertNotNull(user);
+        Assert.assertEquals(expectedUserId, user.getId());
+    }
+}
